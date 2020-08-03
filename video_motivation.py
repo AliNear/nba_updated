f2a5da1ef9595210e06f684c0f656ded450d3cad
Ali Near <gribali94@gmail.com>
2020-08-03 22:57:12 +0100

    Create a dict of country code and their index + country coloring possibility

--- video_motivation.py
+++ video_motivation.py
@@ -1,10 +1,25 @@
 from manimlib.imports import *
 import os
-ASSESTS_PATH = os.getcwd() + '/' + assets + '/'
+import re
+ASSETS_PATH = os.getcwd() + '/projects/nba_update_project/assets/'
+
+
+def get_index_world():
+    """This function gets all countries initials (regex), then creates a dict
+    with integers (0 - len(initials)) as keys
+    """
+    expression = "\"[A-Z]{2}\""
+    data = open(ASSETS_PATH + "main_world.svg").read()
+    matches = re.findall(expression, data)
+    initials = [matches[i].strip('"')
+                for i in range(len(matches)) if i % 2 == 0]
+
+    return dict(zip(initials, range(len(initials))))
+
 
 class MotivationScene(MovingCameraScene):
 
-    class construct(self):
+    def construct(self):
         self.prepare()
         self.draw_map()
         self.show_different_formats()
@@ -14,8 +29,12 @@ class MotivationScene(MovingCameraScene):
     def prepare(self):
         """Assests prepation (mainly the world map and a backgroudn?)
         """
-        pass
-    
+        self.world_map = AvatarSVG(ASSETS_PATH + "main_world.svg")
+        self.world_map.scale(2)
+        self.play(ShowCreation(self.world_map))
+        self.play(self.world_map.color_country, "DZ")
+        self.wait()
+
     def draw_map(self):
         """Draw the world map"""
         pass
@@ -32,3 +51,40 @@ class MotivationScene(MovingCameraScene):
         """Show why it can't be done in the us (big country)"""
         pass
 
+
+class AvatarSVG(SVGMobject):
+    CONFIG = {
+        "fill_opacity": 1.0,
+        "height": 3,
+        "close_new_points": True,
+        "stroke_width": 1,
+        "stroke_color": BLACK,
+    }
+
+    def __init__(self, file_name="dz.svg", **kwargs):
+        self.parts_named = False
+        self.svg_file = file_name
+        SVGMobject.__init__(self, file_name=self.svg_file, **kwargs)
+        self.countries = get_index_world()
+
+    def name_parts(self):
+        self.parts = self.submobjects
+        self.parts_named = True
+
+    def copy(self):
+        copy_mobject = SVGMobject.copy(self)
+        copy_mobject.name_parts()
+        return copy_mobject
+
+    def init_colors(self):
+        SVGMobject.init_colors(self)
+        if not self.parts_named:
+            self.name_parts()
+        for i in self.parts:
+            # i.set_color(RED)
+            self.set_opacity(1)
+        return self
+
+    def color_country(self, country_code, color=RED):
+        country_index = self.countries[country_code]
+        self.parts[country_index].set_color(color)
