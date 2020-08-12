ae2d6dd6daa2ef18c23aa90324761a0b0eecf829
Ali Near <gribali94@gmail.com>
2020-08-12 16:50:09 +0100

    Added count box with Animated Numbers

--- usa_map.py
+++ usa_map.py
@@ -1,5 +1,6 @@
 from manimlib.imports import *
 from projects.project_one.custom_mobjects import *
+from projects.nba_update_project.consts_defs import *
 import os
 ASSETS_PATH = os.getcwd() + "/projects/nba_update_project/assets/"
 MAP_FILE = "us.svg"
@@ -13,18 +14,20 @@ class NumberAnimation(VMobject):
             "font": "Fjalla One",
         },
         "scale_factor": .3,
-        "animation_length": .5
+        "animation_length": 1,
 
     }
 
-    def __init__(self, x, y, **kwargs):
+    def __init__(self, next_to_object, spacing=.3, **kwargs):
         VMobject.__init__(self, **kwargs)
         self.current_value = 0
         self.number = Text("0", **self.text_kwargs)
-        self.number.set_xy(x, y)
-        self.x = x
-        self.y = y
+        self.number.scale(self.scale_factor)
+        self.number.next_to(next_to_object, RIGHT, buff=spacing)
+        self.next_to_object = next_to_object
         self.first = True
+        self.spacing = spacing
+        self.add(self.number)
     def increment(self, amount):
         alpha = ValueTracker(0)
         self.old_alpha = 0
@@ -34,7 +37,8 @@ class NumberAnimation(VMobject):
             new_number = Text(
                     str(int(self.current_value)), **self.text_kwargs)
             new_number.scale(self.scale_factor)
-            new_number.set_xy(self.x, self.y)
+            new_number.next_to(self.next_to_object, RIGHT, buff=self.spacing)
+            #new_number.set_xy(self.x, self.y)
             t.become(new_number)
             self.old_alpha = alpha.get_value()
 
@@ -89,7 +93,7 @@ class NBAScene(MovingCameraScene):
         lower_team_divider = upper_team_divider.copy().set_y(y_lower_teal_divider)
         self.main_dividers = VGroup(left_divider, right_divider)
         self.team_dividers = VGroup(upper_team_divider, lower_team_divider)
-
+        self.wireframe = VGroup(self.main_dividers, self.main_line, self.team_dividers)
     def add_games_count(self, count=3):
         """This method add a table like object to hold different games 
         count, ex: In Division, Other Division, ...
@@ -107,14 +111,29 @@ class NBAScene(MovingCameraScene):
             self.categories.add(category)
             starting_y -= .6
         self.categories.set_xy(starting_x, starting_y)
-        total = Text("Total", font="DDTW00-CondensedBoldItalic", color="#cd8585").scale(.35)
+        total_color = "#cd8585"
+        total = Text("Total", font="DDTW00-CondensedBoldItalic", color=total_color).scale(.35)
         total.set_y(starting_y - 1.2)
         total.set_x(starting_x - total.get_width())
         self.categories.add(total)
         #Thin line 
         self.box_divider = Line(np.array([6, 1.3, 0]), np.array([6, -1., 0]),
                                 color="#979797", stroke_width=.5)
-         
+        #NumberAnimation objects
+        self.numbers = VGroup()
+        for i in range(3):
+            spacing = 3. - self.categories[i].get_width()
+            number = NumberAnimation(self.categories[i], spacing=spacing)
+            self.numbers.add(number)
+        #Not the greatest way to do it, but it works
+        self.total_config = {
+                "text_kwargs": {
+                    "color": total_color,
+                    },
+                "scale_factor": .35,
+                }
+        total = NumberAnimation(self.categories[3], spacing = 2.2, **self.total_config)
+        self.numbers.add(total)
 
 class MainMapScene(MovingCameraScene):
     CONFIG = {
@@ -132,8 +151,9 @@ class MainMapScene(MovingCameraScene):
         self.to_division()
     
     def prepare(self):
-        pass
-    
+        self.map = MapUSA(ASSETS_PATH + "us.svg")
+        self.play(ShowCreation(self.map))
+
     def general_info(self):
         """Give general information about the NBA, ie: number of teams,
         acronym signification, when did it start, ...
@@ -157,13 +177,9 @@ class MainMapScene(MovingCameraScene):
 class Test(NBAScene):
     def construct(self):
         self.play(Write(self.title))
-        self.play(ShowCreation(self.main_line))
-        self.play(ShowCreation(self.main_dividers))
-        self.play(ShowCreation(self.team_dividers))
+        self.play(ShowCreation(self.wireframe))
         self.play(ShowCreation(self.categories))
         self.play(ShowCreation(self.box_divider))
-        a = NumberAnimation(0, 0)
-        self.add(a)
-        self.play(a.increment(5))
+        self.play(ShowCreation(self.numbers))
+        self.play(self.numbers[0].increment(20))
         self.wait()
-        self.play(a.increment(20))
